3528b32ff7640ba8ccd5bb86cdb2bf2c4ced9fb0
Esther <esthernadja@gmail.com>
2023-08-19 14:45:36 -0300

    criando a colisão para a bola, seu launch e o reset de sua posição. também arrumei o collider do paddle, para melhor jogabilidade

--- Historia dos Jogos/Assets/Scripts/brick_break/ball_brick_breaker.cs
+++ Historia dos Jogos/Assets/Scripts/brick_break/ball_brick_breaker.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 public class ball_brick_breaker : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
-
-        rb.AddForce(Vector2.up * 500);
+                
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("bottom"))
+        {
+            Debug.Log("Ball Hit the bottom of the screen");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
     }
 }
